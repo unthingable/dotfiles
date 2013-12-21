ac4b72b52199cff808c8831669fe63abc8437399
unthingable <unthingable@users.noreply.github.com>
2013-12-21 00:01:39 -0800

    multiple cursors and js2-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,7 @@
 (tool-bar-mode nil)
 
 (setq idle-update-delay 0.5) ; not sure what this does yet
-(setq idle-highlight-idle-time 0.1)
+(setq idle-highlight-idle-time 0.5)
 
 ;; enable hax0red packages
 (let ((default-directory "~/.emacs.d/vendor/"))
@@ -70,6 +70,8 @@
 (require 'starter-kit-bindings)
 (require 'starter-kit-lisp)
 ;; (require 'starter-kit-js)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (require 'auto-complete)
 (global-auto-complete-mode t)
@@ -131,6 +133,36 @@ setq my-color-themes (list 'sanityinc-solarized-dark
 (require 'tabbar)
 ; turn on the tabbar
 (tabbar-mode t)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-all-like-this-dwim)
+;; (global-set-key (kbd "C-<") 'mc/mark-previous-word-like-this)
+;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(setq mc/cmds-to-run-for-all
+      '(
+        evil-append-line
+        evil-backward-WORD-begin
+        evil-backward-word-begin
+        evil-delete-char
+        evil-delete-line
+        evil-digit-argument-or-evil-beginning-of-line
+        evil-emacs-state
+        evil-end-of-line
+        evil-force-normal-state
+        evil-forward-WORD-begin
+        evil-forward-WORD-end
+        evil-forward-word-begin
+        evil-forward-word-end
+        evil-insert
+        evil-next-line
+        evil-normal-state
+        evil-previous-line
+        evil-forward-char
+        evil-backward-char
+        kill-region
+        evil-delete-backward-char-and-join
+        ))
 
 
 (require 'dired+)
@@ -295,18 +327,24 @@ setq my-color-themes (list 'sanityinc-solarized-dark
 
 ;; indentation settings
 (setq js-indent-level 2)
+(setq js2-basic-offset 2)
 (electric-indent-mode t)
+;; indent, dammit!
+(define-key js2-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+(setq js2-bounce-indent-p t)
+
+(setq shift-select-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (heroku)))
+ '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
-    ("923faef2c7ed017e63f517703c846c6190c31400261e8abdb1be06d5b46ea19a" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "09feeb867d1ca5c1a33050d857ad6a5d62ad888f4b9136ec42002d6cdf310235" "caa9a86ff9b85f733b424f520ec6ecff3499a36f20eb8d40e3096dbbe1884069" "9dc64d345811d74b5cd0dac92e5717e1016573417b23811b2c37bb985da41da2" "6cf0e8d082a890e94e4423fc9e222beefdbacee6210602524b7c84d207a5dfb5" "6ecfc451f545459728a4a8b1d44ac4cdcc5d93465536807d0cb0647ef2bb12c4" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "929744da373c859c0f07325bc9c8d5cc30d418468c2ecb3a4f6cb2e3728d4775" "5562060e16ae3188e79d87e9ba69d70a6922448bcc5018205850d10696ed0116" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" default)))
- )
+    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "923faef2c7ed017e63f517703c846c6190c31400261e8abdb1be06d5b46ea19a" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "09feeb867d1ca5c1a33050d857ad6a5d62ad888f4b9136ec42002d6cdf310235" "caa9a86ff9b85f733b424f520ec6ecff3499a36f20eb8d40e3096dbbe1884069" "9dc64d345811d74b5cd0dac92e5717e1016573417b23811b2c37bb985da41da2" "6cf0e8d082a890e94e4423fc9e222beefdbacee6210602524b7c84d207a5dfb5" "6ecfc451f545459728a4a8b1d44ac4cdcc5d93465536807d0cb0647ef2bb12c4" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "929744da373c859c0f07325bc9c8d5cc30d418468c2ecb3a4f6cb2e3728d4775" "5562060e16ae3188e79d87e9ba69d70a6922448bcc5018205850d10696ed0116" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" default)))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
