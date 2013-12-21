5faab03c222f67af989d587e6a9367e836fe6c0e
unthingable <unthingable@users.noreply.github.com>
2013-12-20 21:49:47 -0800

    fix and rejigger

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 
 ;; always stay current
 (global-auto-revert-mode t)
+
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
@@ -9,33 +10,52 @@
 (setq make-backup-files nil)
 (setq auto-save-default t)
 
+;; show the menu bar, on osx it costs nothing
 (menu-bar-mode 1)
+(tool-bar-mode nil)
 
-(setq idle-update-delay 0.5)
+(setq idle-update-delay 0.5) ; not sure what this does yet
 (setq idle-highlight-idle-time 0.1)
 
+;; enable hax0red packages
+(let ((default-directory "~/.emacs.d/vendor/"))
+  (normal-top-level-add-subdirs-to-load-path))
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+
+
+;; https://github.com/emacs-jp/replace-colorthemes
+(add-to-list 'custom-theme-load-path
+             (file-name-as-directory "~/.emacs.d/replace-colorthemes/"))
+
+;; (byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
+
 ;; bootstrap packaging
+(setq package-user-dir "~/.emacs.d/elpa-test/")
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ;; MELPA proves useless again.
+                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 
-;; (require 'hl-line+)
-;; (toggle-hl-line-when-idle 1)
-;; (hl-line-when-idle-interval 1.0)
-
 (defvar my-packages '(
-                      starter-kit
-                      starter-kit-lisp
-                      starter-kit-bindings
                       dired+
-;;                      diredx
                       clojure-mode
                       clojure-test-mode
                       hackernews
                       nrepl
                       paredit
+                      projectile
+                      tabbar
+                      flx-ido
+                      ido-ubiquitous
+                      hexrgb
                       ;; cider
                       evil
                       evil-paredit))
@@ -46,29 +66,32 @@
 
 (when (not package-archive-contents) (package-refresh-contents))
 
+(require 'starter-kit)
+(require 'starter-kit-bindings)
+(require 'starter-kit-lisp)
+;; (require 'starter-kit-js)
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(require 'auto-complete-config)
+(ac-config-default)
+
 (require 'linum)
 (line-number-mode 1)
-(column-number-mode 1)  ;; Line numbers on left most column
+;; (column-number-mode 1)  ;; Line numbers on left most column
 (global-linum-mode 1)
 
-;; KTHXBAI! undo some of the starter kit
-;; (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 
-;; (require 'jedi)
-;; (setq jedi:setup-keys t)
-;; (add-hook 'python-mode-hook 'jedi:setup)
+(require 'sr-speedbar)
 
-;; (require 'sr-speedbar)
-
-;; (setq pe/omit-regex "\.\\(pyc\\)$")
-;; (require 'project-explorer)
 
 (require 'projectile)
 (projectile-global-mode)
-;; (setq projectile-require-project-root nil)
 
-(require 'color-theme-sanityinc-solarized)
-(require 'color-theme-sanityinc-tomorrow)
+;; (require 'color-theme)
+;; (color-theme-initialize)
+;; (require 'color-theme-sanityinc-solarized)
+;; (require 'color-theme-sanityinc-tomorrow)
 (;; cyclable themes!
 setq my-color-themes (list 'sanityinc-solarized-dark
                             'sanityinc-solarized-light
@@ -102,7 +125,7 @@ setq my-color-themes (list 'sanityinc-solarized-dark
 ;; this messes with desktop save, so skip
 ;; (my-theme-set-default)
 ;; (global-set-key [f8] 'my-theme-cycle)
-(global-set-key [f8] 'color-theme-select)
+(global-set-key [f8] 'customize-themes)
 
 
 (require 'tabbar)
@@ -113,7 +136,7 @@ setq my-color-themes (list 'sanityinc-solarized-dark
 (require 'dired+)
 (require 'dired-x)
 (setq-default dired-omit-files-p t)
-(setq dired-omit-files "^\\.[^.]\\|\\.pdf$\\|\\.pyc$")
+(setq dired-omit-files "\\.pyc$")
 
 ;; evil stuff ("M-x evil-mode" to toggle)
 (require 'evil)
@@ -133,8 +156,27 @@ setq my-color-themes (list 'sanityinc-solarized-dark
   (toggle-indicate-empty-lines))
 
 
+;; http://stackoverflow.com/questions/9688748/emacs-comment-uncomment-current-line
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+
+(defun comment-sexp ()
+  "Comment out the sexp at point."
+  (interactive)
+  (save-excursion
+    (mark-sexp)
+    (paredit-comment-dwim)))
+
 ;; some keys
-(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "C-M-;") 'comment-sexp)
 (global-set-key (kbd "M-/") 'hippie-expand)
 ;; already defined by starter-kit-bindings
 (global-set-key (kbd "C-+") 'text-scale-increase)
@@ -148,7 +190,6 @@ setq my-color-themes (list 'sanityinc-solarized-dark
 (global-set-key (kbd "s-<down>") 'tabbar-backward-group)
 (global-set-key (kbd "s-[") 'tabbar-backward)
 (global-set-key (kbd "s-]") 'tabbar-forward)
-
 
 ;; saner scrolling
 (setq redisplay-dont-pause t
@@ -182,11 +223,7 @@ setq my-color-themes (list 'sanityinc-solarized-dark
        (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
 
 
-;; makes the square visual bell go away (this is also how we know
-;; everything has loaded)
-(setq ring-bell-function 'ignore)
-;; (add-to-list 'load-path "~/.emacs.d/")
-;; (load "bell")
+(setq ring-bell-function 'ignore) ; shut up!
 
 
 ;; make Emacs tells us everything it does
@@ -223,14 +260,14 @@ setq my-color-themes (list 'sanityinc-solarized-dark
         (let ((ido-ubiquitous-enable-compatibility nil))
           ad-do-it))))
 
-;; (set-face-background hl-line-face "medium purple")
 ;; fix hl-line color dynamically:
 (require 'hexrgb)
 (defun my-fix-hl-line-color ()
   "Adjust hl-line color relative to current color theme"
   (interactive)
   (let*
-      ((value (hexrgb-value color))
+      ((color (face-attribute 'default :background))
+       (value (hexrgb-value color))
        (threshold 0.32)
        (scale 0.9)
        (new-value
@@ -241,15 +278,38 @@ setq my-color-themes (list 'sanityinc-solarized-dark
     (set-face-background
      hl-line-face
      (hexrgb-increment-value
-      (face-attribute 'default :background)
+      color
       (- new-value value)))
     (message "%S %S" value new-value)))
 
-(add-hook 'after-init-hook 'my-fix-hl-line-color)
+;; (my-fix-hl-line-color)
+;; (add-hook 'after-init-hook 'my-fix-hl-line-color)
 
-(when (package-installed-p 'color-theme) 
-  (defadvice color-theme-install-at-point (after fix-hl-line activate)
-    (my-fix-hl-line-color)))
+;; (when (package-installed-p 'color-theme) 
+;;   (defadvice color-theme-install-at-point (after fix-hl-line activate)
+;;     (my-fix-hl-line-color)))
 
-(defadvice load-theme (after fix-hl-line activate)
-  (my-fix-hl-line-color))
+;; (defadvice load-theme (after fix-hl-line activate)
+;;   (my-fix-hl-line-color))
+
+
+;; indentation settings
+(setq js-indent-level 2)
+(electric-indent-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (heroku)))
+ '(custom-safe-themes
+   (quote
+    ("923faef2c7ed017e63f517703c846c6190c31400261e8abdb1be06d5b46ea19a" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "09feeb867d1ca5c1a33050d857ad6a5d62ad888f4b9136ec42002d6cdf310235" "caa9a86ff9b85f733b424f520ec6ecff3499a36f20eb8d40e3096dbbe1884069" "9dc64d345811d74b5cd0dac92e5717e1016573417b23811b2c37bb985da41da2" "6cf0e8d082a890e94e4423fc9e222beefdbacee6210602524b7c84d207a5dfb5" "6ecfc451f545459728a4a8b1d44ac4cdcc5d93465536807d0cb0647ef2bb12c4" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "929744da373c859c0f07325bc9c8d5cc30d418468c2ecb3a4f6cb2e3728d4775" "5562060e16ae3188e79d87e9ba69d70a6922448bcc5018205850d10696ed0116" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" default)))
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
