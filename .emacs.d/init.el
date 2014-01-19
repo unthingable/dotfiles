3811fe5e53af03a1e77db5354ed8da25a90b3f76
unthingable <unthingable@users.noreply.github.com>
2014-01-19 10:04:22 -0800

    autofill, asciidoc, visual line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,8 +15,7 @@
 (require 'package)
 
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
-                         ;; MELPA proves useless again.
-                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
@@ -52,6 +51,7 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; (package-refresh-contents)
 (when (not package-archive-contents) (package-refresh-contents))
 
 (require 'starter-kit)
@@ -71,9 +71,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.asciidoc\\'" . adoc-mode))
 ;; (setq auto-mode-alist (rassq-delete-all 'visual-line-mode auto-mode-alist))
-(add-hook 'markdown-mode-hook (lambda () (visual-line-mode t)))
+(add-hook 'markdown-mode-hook (lambda ()
+                                ;; (visual-line-mode t)
+                                (turn-off-evil-mode)))
+(add-hook 'adoc-mode-hook (lambda () (visual-line-mode t)))
 (auto-fill-mode -1)
+(remove-hook 'text-mode-hook 'turn-on-auto-fill)
 (setq line-move-visual nil)
 
 ;; ag
@@ -81,18 +86,19 @@
 ;; (setq ag-reuse-buffers nil)
 
 ;; Autocomplete
-(setq ac-use-fuzzy t)
 (require 'auto-complete)
 (global-auto-complete-mode t)
 (require 'auto-complete-config)
+(setq ac-use-fuzzy t)
 (ac-config-default)
 
 ;; Line numbers
-(require 'linum)
-(line-number-mode 1)
+;; (require 'linum)
+;; (line-number-mode 1)
 ;; (column-number-mode 1)  ;; Line numbers on left most column
-(global-linum-mode 1)
+;; (global-linum-mode 1)
 
+(add-hook 'prog-mode-hook 'linum-mode)
 
 ;; (require 'sr-speedbar)
 ;; (setq sr-speedbar-auto-refresh t)
@@ -166,6 +172,9 @@
 (add-hook 'prog-mode-hook 'turn-on-evil-mode)
 (add-hook 'comint-mode-hook 'turn-on-evil-mode)
 (add-hook 'Info-mode-hook 'turn-off-evil-mode)
+(add-hook 'org-mode-hook 'turn-off-evil-mode)
+(add-hook 'direx-mode-hook 'turn-off-evil-mode)
+
 (setq evil-default-cursor t)
 (add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode)
 ;; evil doesn't always understant indent by default
@@ -198,7 +207,7 @@
 (defun projectile-find-tag-interactive (tagname)
   (interactive (find-tag-interactive "Pfind tag: "))
   (visit-tags-table (projectile-project-root) t)
-  (tags-completion-table)
+  ;; (tags-completion-table)
   (find-tag tagname))
 
 
@@ -323,7 +332,7 @@
 ;; (my-fix-hl-line-color)
 ;; (add-hook 'after-init-hook 'my-fix-hl-line-color)
 
-;; (when (package-installed-p 'color-theme) 
+;; (when (package-installed-p 'color-theme)
 ;;   (defadvice color-theme-install-at-point (after fix-hl-line activate)
 ;;     (my-fix-hl-line-color)))
 
@@ -387,7 +396,7 @@ Return a list of one element based on major mode."
      )
     (t
      ;; Return `mode-name' if not blank, `major-mode' otherwise.
-     (let ((group 
+     (let ((group
             (if (and (stringp mode-name)
                      ;; Take care of preserving the match-data because this
                      ;; function is called when updating the header line.
@@ -435,15 +444,76 @@ Return a list of one element based on major mode."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (deep-blue)))
+ '(Linum-format "%7i ")
+ '(ag-arguments
+   (quote
+    ("--smart-case" "--nogroup" "--ignore" "TAGS" "--column" "--")))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#110F13" "#b13120" "#719f34" "#ceae3e" "#7c9fc9" "#7868b5" "#009090" "#F4EAD5"])
+ '(ansi-term-color-vector
+   [unspecified "#110F13" "#b13120" "#719f34" "#ceae3e" "#7c9fc9" "#7868b5" "#009090" "#F4EAD5"])
+ '(background-color "#042028")
+ '(background-mode dark)
+ '(cursor-color "#708183")
+ '(custom-enabled-themes (quote (my-deeper-blue)))
  '(custom-safe-themes
    (quote
-    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "31772cd378fd8267d6427cec2d02d599eee14a1b60e9b2b894dd5487bd30978e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "180adb18379d7720859b39124cb6a79b4225d28cef4bfcf4ae2702b199a274c8" "16e7c7811fd8f1bc45d17af9677ea3bd8e028fce2dd4f6fa5e6535dea07067b1" "f7c428459d84a94cd3d4e8d9a7aa3178a13ee0092d91d5e4a4fce4c2b1242934" "617219c11282b84761477059b9339da78ce392c974d9308535ee4ec8c0770bee" "579e9950513524d8739e08eae289419cfcb64ed9b7cc910dd2e66151c77975c4" "89f613708c8018d71d97e3da7a1e23c8963b798252f1ac2ab813ad63b7a4b341" "394504bd559027641b544952d6e9e1c6dcb306b4d1b2c4ad6b98d3e6b5459683" "3dd173744ae0990dd72094caef06c0b9176b3d98f0ee5d822d6a7e487c88d548" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "e83c94a6bfab82536cef63610ec58d08dfddd27752d860763055daf58d028aad" "f211f8db2328fb031908c9496582e7de2ae8abd5f59a27b4c1218720a7d11803" "2c73700ef9c2c3aacaf4b65a7751b8627b95a1fd8cebed8aa199f2afb089a85f" "787574e2eb71953390ed2fb65c3831849a195fd32dfdd94b8b623c04c7f753f0" "e890fd7b5137356ef5b88be1350acf94af90d9d6dd5c234978cd59a6b873ea94" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "246a51f19b632c27d7071877ea99805d4f8131b0ff7acb8a607d4fd1c101e163" "1177fe4645eb8db34ee151ce45518e47cc4595c3e72c55dc07df03ab353ad132" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "923faef2c7ed017e63f517703c846c6190c31400261e8abdb1be06d5b46ea19a" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "09feeb867d1ca5c1a33050d857ad6a5d62ad888f4b9136ec42002d6cdf310235" "caa9a86ff9b85f733b424f520ec6ecff3499a36f20eb8d40e3096dbbe1884069" "9dc64d345811d74b5cd0dac92e5717e1016573417b23811b2c37bb985da41da2" "6cf0e8d082a890e94e4423fc9e222beefdbacee6210602524b7c84d207a5dfb5" "6ecfc451f545459728a4a8b1d44ac4cdcc5d93465536807d0cb0647ef2bb12c4" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "929744da373c859c0f07325bc9c8d5cc30d418468c2ecb3a4f6cb2e3728d4775" "5562060e16ae3188e79d87e9ba69d70a6922448bcc5018205850d10696ed0116" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" default)))
+    ("1a8f59a0b5769efb44cb727a799354e0851e7509d5160676cc01eaf74434990f" "ec0c9d1715065a594af90e19e596e737c7b2cdaa18eb1b71baf7ef696adbefb0" "0ff3aeed353697992d100ddf8a94d065a58ffbde5a40afefa605f211757c8ab0" "1faffcddc50d5dc7d334f2817dd6f159ef1820be3aad303eb7f74006531afdff" "31ba13fd560daff5b05e11d4be7d280213249225e85969ec5bc71532e788ee81" "fc89666d6de5e1d75e6fe4210bd20be560a68982da7f352bd19c1033fb7583ba" "3fe4861111710e42230627f38ebb8f966391eadefb8b809f4bfb8340a4e85529" "6e03b7f86fcca5ce4e63cda5cd0da592973e30b5c5edf198eddf51db7a12b832" "c712d616ea5a9ef4e513681846eb908728bbb087c2d251ded8374ee9faafa199" "af4cfe7f2de40f19e0798d46057aae0bccfbc87a85a2d4100339eaf91a1f202a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "31772cd378fd8267d6427cec2d02d599eee14a1b60e9b2b894dd5487bd30978e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "180adb18379d7720859b39124cb6a79b4225d28cef4bfcf4ae2702b199a274c8" "16e7c7811fd8f1bc45d17af9677ea3bd8e028fce2dd4f6fa5e6535dea07067b1" "f7c428459d84a94cd3d4e8d9a7aa3178a13ee0092d91d5e4a4fce4c2b1242934" "617219c11282b84761477059b9339da78ce392c974d9308535ee4ec8c0770bee" "579e9950513524d8739e08eae289419cfcb64ed9b7cc910dd2e66151c77975c4" "89f613708c8018d71d97e3da7a1e23c8963b798252f1ac2ab813ad63b7a4b341" "394504bd559027641b544952d6e9e1c6dcb306b4d1b2c4ad6b98d3e6b5459683" "3dd173744ae0990dd72094caef06c0b9176b3d98f0ee5d822d6a7e487c88d548" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "e83c94a6bfab82536cef63610ec58d08dfddd27752d860763055daf58d028aad" "f211f8db2328fb031908c9496582e7de2ae8abd5f59a27b4c1218720a7d11803" "2c73700ef9c2c3aacaf4b65a7751b8627b95a1fd8cebed8aa199f2afb089a85f" "787574e2eb71953390ed2fb65c3831849a195fd32dfdd94b8b623c04c7f753f0" "e890fd7b5137356ef5b88be1350acf94af90d9d6dd5c234978cd59a6b873ea94" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "246a51f19b632c27d7071877ea99805d4f8131b0ff7acb8a607d4fd1c101e163" "1177fe4645eb8db34ee151ce45518e47cc4595c3e72c55dc07df03ab353ad132" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "923faef2c7ed017e63f517703c846c6190c31400261e8abdb1be06d5b46ea19a" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "085b401decc10018d8ed2572f65c5ba96864486062c0a2391372223294f89460" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "09feeb867d1ca5c1a33050d857ad6a5d62ad888f4b9136ec42002d6cdf310235" "caa9a86ff9b85f733b424f520ec6ecff3499a36f20eb8d40e3096dbbe1884069" "9dc64d345811d74b5cd0dac92e5717e1016573417b23811b2c37bb985da41da2" "6cf0e8d082a890e94e4423fc9e222beefdbacee6210602524b7c84d207a5dfb5" "6ecfc451f545459728a4a8b1d44ac4cdcc5d93465536807d0cb0647ef2bb12c4" "f831c1716ebc909abe3c851569a402782b01074e665a4c140e3e52214f7504a0" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "929744da373c859c0f07325bc9c8d5cc30d418468c2ecb3a4f6cb2e3728d4775" "5562060e16ae3188e79d87e9ba69d70a6922448bcc5018205850d10696ed0116" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" default)))
  '(direx:closed-icon "+ ")
  '(direx:open-icon "- ")
+ '(exec-path
+   (quote
+    ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.3.50/x86_64-apple-darwin13.0.0" "/usr/local/bin")))
+ '(fci-rule-character-color "#202020")
+ '(fci-rule-color "#202020")
+ '(foreground-color "#708183")
+ '(frame-brackground-mode (quote dark))
+ '(fringe-mode 4 nil (fringe))
  '(help-window-select t)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors
+   (quote
+    (("#F2F2F2" . 0)
+     ("#B4C342" . 20)
+     ("#69CABF" . 30)
+     ("#6DA8D2" . 50)
+     ("#DEB542" . 60)
+     ("#F2804F" . 70)
+     ("#F771AC" . 85)
+     ("#F2F2F2" . 100))))
  '(js2-basic-offset 2)
+ '(linum-delay t)
+ '(linum-eager nil)
+ '(linum-format "%3i")
+ '(main-line-color1 "#1e1e1e")
+ '(main-line-color2 "#111111")
+ '(main-line-separator-style (quote chamfer))
+ '(powerline-color1 "#1e1e1e")
+ '(powerline-color2 "#111111")
  '(projectile-tags-command "/usr/local/bin/ctags -Re %s")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#c60007")
+     (40 . "#bd3612")
+     (60 . "#a57705")
+     (80 . "#728a05")
+     (100 . "#259185")
+     (120 . "#2075c7")
+     (140 . "#c61b6e")
+     (160 . "#5859b7")
+     (180 . "#c60007")
+     (200 . "#bd3612")
+     (220 . "#a57705")
+     (240 . "#728a05")
+     (260 . "#259185")
+     (280 . "#2075c7")
+     (300 . "#c61b6e")
+     (320 . "#5859b7")
+     (340 . "#c60007")
+     (360 . "#bd3612"))))
  '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -473,6 +543,8 @@ Return a list of one element based on major mode."
     (desktop-save desktop-dirname))
 (add-hook 'auto-save-hook 'my-desktop-save)
 (add-hook 'kill-emacs-hook 'my-desktop-save)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; global behavior
 
@@ -602,3 +674,32 @@ Return a list of one element based on major mode."
        (car recently-killed-list)))))
 
 ;; (load-file "pelican.el")
+
+;; (require 'org-freemind)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-directory "~/Documents/org")
+(setq org-default-notes-file "~/Documents/org/refile.org")
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file org-default-notes-file)
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file org-default-notes-file)
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file org-default-notes-file)
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree (concat org-dir "/diary.org"))
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file org-default-notes-file)
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file org-default-notes-file)
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file org-default-notes-file)
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file org-default-notes-file)
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"<%Y-%m-%d %a .+1d/3d>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
